5972c703d5fff4d9c9c87299461086be27e73e95
Alex Watts <alexjwatts83@gmail.com>
2022-07-22 07:47:24 +1000

    testing out forms

--- test-angular-14-app/src/app/app.component.ts
+++ test-angular-14-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, UntypedFormControl } from '@angular/forms';
+import { first, last } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-angular-14-app';
+
+  /**
+   *
+   */
+  constructor() {
+  }
+  ngOnInit(): void {
+    const cat = new FormGroup({
+      lives: new FormControl(9)
+   });
+  }
 }
