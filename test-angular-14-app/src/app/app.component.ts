93cb0f3ee6b1f633c7fceeb725a9abf686410464
Alex Watts <alexjwatts83@gmail.com>
2022-07-26 08:05:22 +1000

    more form stuff

--- test-angular-14-app/src/app/app.component.ts
+++ test-angular-14-app/src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, UntypedFormControl } from '@angular/forms';
 import { first, last } from 'rxjs';
 
+export interface ContactForm {
+  name: FormControl<string>;
+  email: FormControl<string>;
+  contactNumber?: FormControl<number | null>;
+  query?: FormControl<string | null>;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +16,11 @@ import { first, last } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   title = 'test-angular-14-app';
-  cat = new FormGroup({
-    lives: new UntypedFormControl(9),
+  contactForm = new FormGroup<ContactForm>({
+    name: new FormControl<string>('', { nonNullable: true }),
+    email: new FormControl<string>('', { nonNullable: true}),
+    contactNumber: new FormControl<number>(0, { nonNullable: false}),
+    query:new FormControl<string>('I would like to connect!', { nonNullable: false })
   });
   /**
    *
