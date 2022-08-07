342a27840cdb1d5ed5f57493c7dce20be11091cc
Alex Watts <alexjwatts83@gmail.com>
2022-08-08 07:11:58 +1000

    added form record component

--- test-angular-14-app/src/app/app.module.ts
+++ test-angular-14-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TypedFormComponent } from './typed-form/typed-form.component';
 import { UntypedFormComponent } from './untyped-form/untyped-form.component';
+import { FormRecordComponent } from './form-record/form-record.component';
 
 @NgModule({
-  declarations: [		
+  declarations: [			
     AppComponent,
       TypedFormComponent,
-      UntypedFormComponent
+      UntypedFormComponent,
+      FormRecordComponent
    ],
   imports: [
     BrowserModule,
