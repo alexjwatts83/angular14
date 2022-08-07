9a2843cbc97ece6d88892292272779c7901c3d92
Alex Watts <alexjwatts83@gmail.com>
2022-08-08 07:15:07 +1000

    wip form array component

--- test-angular-14-app/src/app/app.module.ts
+++ test-angular-14-app/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { TypedFormComponent } from './typed-form/typed-form.component';
 import { UntypedFormComponent } from './untyped-form/untyped-form.component';
 import { FormRecordComponent } from './form-record/form-record.component';
+import { FormArrayComponent } from './form-array/form-array.component';
 
 @NgModule({
-  declarations: [			
+  declarations: [				
     AppComponent,
       TypedFormComponent,
       UntypedFormComponent,
-      FormRecordComponent
+      FormRecordComponent,
+      FormArrayComponent
    ],
   imports: [
     BrowserModule,
