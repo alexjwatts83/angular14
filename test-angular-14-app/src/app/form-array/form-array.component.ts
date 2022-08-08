fd92a7ca888d9c95f0c8c37026f85c425acfaf03
Alex Watts <alexjwatts83@gmail.com>
2022-08-09 07:23:01 +1000

    moved code

--- test-angular-14-app/src/app/form-array/form-array.component.ts
+++ test-angular-14-app/src/app/form-array/form-array.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-form-array',
   templateUrl: './form-array.component.html',
-  styleUrls: ['./form-array.component.scss']
+  styleUrls: ['./form-array.component.scss'],
 })
 export class FormArrayComponent implements OnInit {
+  skillsForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  get skills(): FormArray {
+    return this.skillsForm.get('skills') as FormArray;
   }
 
+  constructor(private fb: FormBuilder) {
+    this.skillsForm = this.fb.group({
+      name: '',
+      skills: this.fb.array([]),
+    });
+  }
+
+  ngOnInit() {}
 }
